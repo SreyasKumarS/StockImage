a0fb274cbd8b9b31d29713c073271124fd6a78ec
SreyasKumarS <sreyassree3333@gmail.com>
2025-01-27 17:27:24 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,5 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server runningg on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
